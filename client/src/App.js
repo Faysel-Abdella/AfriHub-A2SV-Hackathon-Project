e2cd6172570c10e8a651c7dfd011fa9dcbb35a78
faysel abdella <fayselcode@gmail.com>
2023-09-02 02:40:47 -0700

    signup page is created

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/HomePage";
-import ErrorPage from "./pages/404";
 import SignupPage from "./pages/SignupPage";
 
-// import { action as completeSignupAction } from "./pages/CompleteSignupPage";
-// import { action as loginAction } from "./pages/LoginPage";
+import ErrorPage from "./pages/404";
+
+// import { loader as dashboardPageLoader } from "./pages/EditProfilePage";
 
 const router = createBrowserRouter([
   {
@@ -15,27 +15,13 @@ const router = createBrowserRouter([
     element: <HomePage />,
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <HomePage /> },
+      { index: true, element: <SignupPage /> },
       {
-        path: "signup",
-        element: <SignupPage />,
-        // action: completeSignupAction,
+        path: "complete-signup",
       },
       // { path: "login", element: <LoginPage />, action: loginAction },
     ],
   },
-  // {
-  //   path: "dashboard",
-  //   element: <Dashboard />,
-  //   // loader: dashboardPageLoader,
-  //   children: [
-  //     { index: true, element: <TasksPage /> },
-  //     {
-  //       path: "edit-profile",
-  //       element: <EditProfilePage />,
-  //     },
-  //   ],
-  // },
 ]);
 
 function App() {
--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -15,51 +15,18 @@ import { BsEye } from "react-icons/bs";
 // import { toast } from "react-toastify";
 
 const SignupPage = () => {
-  const navigate = useNavigate();
-
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
   const togglePasswordVisible = () => setPasswordVisible(!passwordVisible);
   const toggleConfirmPasswordVisible = () =>
     setConfirmPasswordVisible(!confirmPasswordVisible);
 
-  const dispatch = useDispatch();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-
-  const [isSigning, setIsSigning] = useState(false);
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleConfirmPasswordChange = (event) => {
-    setConfirmPassword(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setIsSigning(true);
-
-    const request = {
-      email: email,
-      password: password,
-      confirmPassword: confirmPassword,
-    };
-  };
-
   return (
     <Wrapper>
       <div className="signup-container">
         <div className="inside-singup">
           <h1>Signup</h1>
-          <Form method="POST" onSubmit={handleSubmit}>
+          <Form method="POST">
             <FormRow name="name" type="text" label="Name" />
             <FormRow name="lastName" type="text" label="Last Name" />
             <FormRow name="email" type="text" label="Email" />
@@ -69,7 +36,6 @@ const SignupPage = () => {
               label="Password"
               EyeIcon={passwordVisible ? BsEye : BsEyeSlash}
               togglePassword={togglePasswordVisible}
-              onChange={handlePasswordChange}
             />
             <FormRow
               name="confirmPassword"
@@ -77,7 +43,6 @@ const SignupPage = () => {
               label="Confirm password"
               EyeIcon={confirmPasswordVisible ? BsEye : BsEyeSlash}
               togglePassword={toggleConfirmPasswordVisible}
-              onChange={handleConfirmPasswordChange}
             />
             {/* <CtaButton
               text={` ${isSigning ? "Signing,  wait..." : "Complete signup"} `}
