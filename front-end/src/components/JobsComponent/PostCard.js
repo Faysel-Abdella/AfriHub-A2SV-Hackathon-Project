f1313e38f5a8bcf089617753d1f9534cedc8718a
Fasill <fasilhawultie19@gmail.com>
2023-09-03 15:43:32 +0300

    job post modified

--- front-end/src/components/JobsComponent/PostCard.js
+++ front-end/src/components/JobsComponent/PostCard.js
@@ -21,7 +21,7 @@ const PostCard = ({ title, description, skills, location, postDate, deadline, pr
     <div className="card my-5 px-3">
     <div className="title border-bottom d-flex justify-content-between align-items-center">
         <h4 className="py-2 text-success large-normal">{ title }</h4>
-        <h4 className="px-3 py-2 text-warning large">{ price } BIRR</h4>
+        <h4 className="px-3 py-2 text-warning large">{ price }</h4>
     </div>
     <div className="description py-3">
         <p className="paragraph">{description}</p>
